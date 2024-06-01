a2e2f75d082f39136f27f990597fd72355e0a063
ayushgupta2959 <ayushgupta2959@gmail.com>
2024-06-01 17:36:12 +0530

    updated deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,32 @@
-const hre = require("hardhat")
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
 
+  const [deployer] = await ethers.getSigners();
+  const NAME = "Metachat";
+  const SYMBOL = "MCH";
+
+  const Metachat = await ethers.getContractFactory("Metachat");
+  const metachat = await Metachat.deploy(NAME, SYMBOL);
+  await metachat.deployed();
+
+  console.log(`Deployed Metachat Contract at: ${metachat.address}\n`);
+
+  const CHANNEL_NAMES = ["general", "intro", "jobs"];
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)];
+
+  for (var i = 0; i < 3; i++) {
+    const transaction = await metachat
+      .connect(deployer)
+      .createChannel(CHANNEL_NAMES[i], COSTS[i]);
+    await transaction.wait();
+
+    console.log(`Created text channel #${CHANNEL_NAMES[i]}`);
+  }
 }
 
 main().catch((error) => {
