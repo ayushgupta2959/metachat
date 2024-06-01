3ad5f9c264314d584150d12c32b607f9eb7e2d99
ayushgupta2959 <ayushgupta2959@gmail.com>
2024-06-01 17:23:15 +0530

    added create and get channel

--- contracts/Metachat.sol
+++ contracts/Metachat.sol
@@ -5,9 +5,32 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Metachat is ERC721 {
     address public owner;
+    uint256 public totalChannels;
+
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping(uint256 => Channel) public channels;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function createChannel(string memory _name, uint256 _cost) public onlyOwner {
+        totalChannels++;
+        channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+        return channels[_id];
     }
 
 }
--- test/Metachat.js
+++ test/Metachat.js
@@ -11,15 +11,24 @@ describe("Metachat", function () {
 
   const NAME = "Metachat";
   const SYMBOL = "MCH";
+  const CHANNEL_ID = 1;
+  const CHANNEL_NAME = "general";
+  const CHANNEL_COST = tokens(1);
 
   beforeEach(async () => {
     // Setup accounts
-    [deployer, user] = await ethers.getSigners()
+    [deployer, user] = await ethers.getSigners();
 
     // Deploy contract
-    const Metachat = await ethers.getContractFactory("Metachat")
-    metachat = await Metachat.deploy(NAME, SYMBOL)
-  })
+    const Metachat = await ethers.getContractFactory("Metachat");
+    metachat = await Metachat.deploy(NAME, SYMBOL);
+
+    // Create a channel
+    const transaction = await metachat
+      .connect(deployer)
+      .createChannel(CHANNEL_NAME, CHANNEL_COST);
+    await transaction.wait();
+  });
 
   describe("Deployment", function () {
     it("Sets the name", async () => {
@@ -35,6 +44,20 @@ describe("Metachat", function () {
     it("Sets the owner", async () => {
       const result = await metachat.owner();
       expect(result).to.equal(deployer.address);
+    });
+  });
+
+  describe("Creating Channels", () => {
+    it("Returns total channels", async () => {
+      const result = await metachat.totalChannels();
+      expect(result).to.be.equal(1);
+    });
+
+    it("Returns channel attributes", async () => {
+      const channel = await metachat.getChannel(CHANNEL_ID);
+      expect(channel.id).to.be.equal(CHANNEL_ID);
+      expect(channel.name).to.be.equal(CHANNEL_NAME);
+      expect(channel.cost).to.be.equal(CHANNEL_COST);
     });
   });
 });
